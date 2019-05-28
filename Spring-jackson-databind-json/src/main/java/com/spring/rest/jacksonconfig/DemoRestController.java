8bde239ba82649f69f25a3cce35cb906ae5dfedf
soumyadip007 <soumyadip.note@gmail.com>
2019-05-29 00:13:55 +0530

    Rest Controller added

---
+++ Spring-jackson-databind-json/src/main/java/com/spring/rest/jacksonconfig/DemoRestController.java
@@ -0,0 +1,17 @@
+package com.spring.rest.jacksonconfig;
+
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/test")
+public class DemoRestController {
+
+	
+	@GetMapping("/hello")
+	public String hello()
+	{
+		return "Hello World";
+	}
+}
