549b2823e53d287a3d27d45014c0e29312851e40
soumyadip007 <soumyadip.note@gmail.com>
2019-05-28 22:09:45 +0530

    Entity added

--- Spring-jackson-databind-json/src/main/java/com/spring/jackson/json/Student.java
+++ Spring-jackson-databind-json/src/main/java/com/spring/jackson/json/Student.java
@@ -2,4 +2,46 @@ package com.spring.jackson.json;
 
 public class Student {
 
+	private int id;
+	private String firstname;
+	private String lastname;
+	private boolean active;
+	
+	public Student() {
+		
+	}
+
+	public int getId() {
+		return id;
+	}
+
+	public void setId(int id) {
+		this.id = id;
+	}
+
+	public String getFirstname() {
+		return firstname;
+	}
+
+	public void setFirstname(String firstname) {
+		this.firstname = firstname;
+	}
+
+	public String getLastname() {
+		return lastname;
+	}
+
+	public void setLastname(String lastname) {
+		this.lastname = lastname;
+	}
+
+	public boolean isActive() {
+		return active;
+	}
+
+	public void setActive(boolean active) {
+		this.active = active;
+	}
+	
+	
 }
