deb24583e0c27e5566bcad8f61fbc4e092714902
soumyadip007 <soumyadip.note@gmail.com>
2019-05-29 00:11:30 +0530

    Config class added

---
+++ Spring-jackson-databind-json/src/main/java/com/spring/rest/jacksonconfig/DemoAppConfig.java
@@ -0,0 +1,14 @@
+package com.spring.rest.jacksonconfig;
+
+import org.springframework.context.annotation.ComponentScan;
+import org.springframework.context.annotation.Configuration;
+import org.springframework.web.servlet.config.annotation.EnableWebMvc;
+
+@Configuration			//Replacement of web.xml
+@EnableWebMvc			//Replacement of spring-mvc-demo.xml
+@ComponentScan(basePackages="com.spring")
+public class DemoAppConfig {
+
+
+	
+}
