cc80ddcc5a813aba991279707873fd0c9c844f9f
soumyadip007 <soumyadip.note@gmail.com>
2019-05-28 22:39:25 +0530

    check added

--- Spring-jackson-databind-json/src/main/java/com/spring/jackson/json/test/Driver.java
+++ Spring-jackson-databind-json/src/main/java/com/spring/jackson/json/test/Driver.java
@@ -21,14 +21,17 @@ public class Driver {
 			Student obj=mapper.readValue(new File("data/sample-full.json"), Student.class);
 			
 			System.out.println("First name :=" + obj.getFirstname());
+			System.out.println("Last name = " + obj.getLastname());		
+
+			// get nested object: array
+			Address tempAddress = obj.getAddress();
+			System.out.println("Street = " + tempAddress.getStreet());		
+			System.out.println("City = " + tempAddress.getCity());		
 			
-			
+		} catch (Exception e) {
+			// TODO Auto-generated catch block
+			e.printStackTrace();
 		}
-		catch(Exception e)
-		{
-			
-		}
-		
 	}
-	
+
 }
