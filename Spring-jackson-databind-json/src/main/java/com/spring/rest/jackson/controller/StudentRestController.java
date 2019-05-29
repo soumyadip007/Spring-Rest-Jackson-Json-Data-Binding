f5bde709561bfd57519f058493f61567e6bb2f97
soumyadip007 <soumyadip.note@gmail.com>
2019-05-29 22:12:54 +0530

    List Added

--- Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/StudentRestController.java
+++ Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/StudentRestController.java
@@ -1,10 +1,22 @@
 package com.spring.rest.jackson.controller;
 
+import java.util.List;
+
+import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
+
+import com.spring.rest.jackson.entity.Student;
 
 @RestController
 @RequestMapping("/api")
 public class StudentRestController {
 
+	
+	
+	@GetMapping("/students")
+	public List<Student> getStudents()
+	{
+		return null;
+	}
 }
