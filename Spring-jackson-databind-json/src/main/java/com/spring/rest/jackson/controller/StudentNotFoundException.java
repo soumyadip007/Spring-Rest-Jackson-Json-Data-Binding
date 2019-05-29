058464b20cb5a51f942a70c15c504f28b9cfcf2b
soumyadip007 <soumyadip.note@gmail.com>
2019-05-29 23:06:12 +0530

    Constructor of the SUPER class added

--- Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/StudentNotFoundException.java
+++ Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/StudentNotFoundException.java
@@ -1,5 +1,19 @@
 package com.spring.rest.jackson.controller;
 
-public class StudentNotFoundException {
+public class StudentNotFoundException extends RuntimeException {
 
+	public StudentNotFoundException(String message, Throwable cause) {
+		super(message, cause);
+	}
+
+	public StudentNotFoundException(String message) {
+		super(message);
+	}
+
+	public StudentNotFoundException(Throwable cause) {
+		super(cause);
+	}
+
+
+	
 }
