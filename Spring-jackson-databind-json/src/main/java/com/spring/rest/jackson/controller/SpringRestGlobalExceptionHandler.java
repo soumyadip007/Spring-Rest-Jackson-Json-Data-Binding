608fb59ff8bb805fb113d8966edd93a9d3a0969f
soumyadip007 <soumyadip.note@gmail.com>
2019-05-30 00:43:05 +0530

    Global controller TAG added

--- Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/SpringRestGlobalExceptionHandler.java
+++ Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/SpringRestGlobalExceptionHandler.java
@@ -5,7 +5,7 @@ import org.springframework.http.ResponseEntity;
 import org.springframework.web.bind.annotation.ControllerAdvice;
 import org.springframework.web.bind.annotation.ExceptionHandler;
 
-@ControllerAdvice
+@ControllerAdvice        //Global controller
 public class SpringRestGlobalExceptionHandler {
 
 	//Add exception Handeler
