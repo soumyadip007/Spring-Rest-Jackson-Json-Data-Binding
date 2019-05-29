4943c376d700ddf433e7313f81c566bb5c237cad
soumyadip007 <soumyadip.note@gmail.com>
2019-05-29 23:03:26 +0530

    StudentErrorResponse method added

--- Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/StudentErrorResponse.java
+++ Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/StudentErrorResponse.java
@@ -6,5 +6,24 @@ public class StudentErrorResponse {
 	private String message;
 	private long timeStamp;
 	
+	public int getStatus() {
+		return status;
+	}
+	public void setStatus(int status) {
+		this.status = status;
+	}
+	public String getMessage() {
+		return message;
+	}
+	public void setMessage(String message) {
+		this.message = message;
+	}
+	public long getTimeStamp() {
+		return timeStamp;
+	}
+	public void setTimeStamp(long timeStamp) {
+		this.timeStamp = timeStamp;
+	}
+	
 	
 }
