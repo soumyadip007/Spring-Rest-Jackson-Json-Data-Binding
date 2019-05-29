4cc9de6b3bd8613c2a22d43b2f95ac7979a39c41
soumyadip007 <soumyadip.note@gmail.com>
2019-05-30 00:21:02 +0530

    Global exception Class added

---
+++ Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/SpringRestGlobalExceptionHandler.java
@@ -0,0 +1,8 @@
+package com.spring.rest.jackson.controller;
+
+import org.springframework.web.bind.annotation.ControllerAdvice;
+
+@ControllerAdvice
+public class SpringRestGlobalExceptionHandler {
+
+}
