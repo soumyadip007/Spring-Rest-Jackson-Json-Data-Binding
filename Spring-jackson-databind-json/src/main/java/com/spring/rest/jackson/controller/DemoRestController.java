a064799dfdb3075502e4921bd657b34297f42908
soumyadip007 <soumyadip.note@gmail.com>
2019-05-29 22:02:57 +0530

    Controller moved

--- Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/DemoRestController.java
+++ Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/DemoRestController.java
@@ -1,4 +1,4 @@
-package com.spring.rest.jackson.config;
+package com.spring.rest.jackson.controller;
 
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
