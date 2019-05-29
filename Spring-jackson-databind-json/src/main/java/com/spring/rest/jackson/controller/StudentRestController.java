ccac0674a05675bf7d53728bacd046531e658ddc
soumyadip007 <soumyadip.note@gmail.com>
2019-05-29 23:17:50 +0530

    Error entity added

--- Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/StudentRestController.java
+++ Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/StudentRestController.java
@@ -5,6 +5,9 @@ import java.util.List;
 
 import javax.annotation.PostConstruct;
 
+import org.springframework.http.HttpStatus;
+import org.springframework.http.ResponseEntity;
+import org.springframework.web.bind.annotation.ExceptionHandler;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.PathVariable;
 import org.springframework.web.bind.annotation.RequestMapping;
@@ -65,4 +68,21 @@ public class StudentRestController {
 		
 	}
 	
+	
+	// Add an exception handeler using @ExceptionHandeler
+	
+	@ExceptionHandler
+	public ResponseEntity<StudentErrorResponse> handleException(StudentNotFoundException	 exe)
+	{
+		//Create a 	StudentErrorResponse
+		StudentErrorResponse error=new StudentErrorResponse();
+		
+		error.setStatus(HttpStatus.NOT_FOUND.value());
+		error.setMessage(HttpStatus.NOT_FOUND.value());
+		error.setTimeStamp(HttpStatus.NOT_FOUND.value());
+		
+		
+		return null;
+		
+	}
 }
