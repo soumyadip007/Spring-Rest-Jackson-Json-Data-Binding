2641e6dfd216435de660b9571ad731f90ab0a537
soumyadip007 <soumyadip.note@gmail.com>
2019-05-29 22:11:45 +0530

    @RestController @RequestMapping("/api") Added

--- Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/StudentRestController.java
+++ Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/StudentRestController.java
@@ -1,5 +1,10 @@
 package com.spring.rest.jackson.controller;
 
+import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
+
+@RestController
+@RequestMapping("/api")
 public class StudentRestController {
 
 }
