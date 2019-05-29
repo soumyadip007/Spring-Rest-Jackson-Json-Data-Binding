0c57793a173b806031023e62f6bc3c20999e9bc9
soumyadip007 <soumyadip.note@gmail.com>
2019-05-29 22:18:14 +0530

    Changed to rest Controller

--- Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/StudentRestController.java
+++ Spring-jackson-databind-json/src/main/java/com/spring/rest/jackson/controller/StudentRestController.java
@@ -3,13 +3,13 @@ package com.spring.rest.jackson.controller;
 import java.util.ArrayList;
 import java.util.List;
 
-import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RestController;
 
 import com.spring.rest.jackson.entity.Student;
 
-@Controller
+@RestController
 @RequestMapping("/api")
 public class StudentRestController {
 
